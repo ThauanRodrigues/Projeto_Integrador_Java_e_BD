ed061d1f839028b9a4b9c6c99428abc283bb1582
Thauan Rodrigues <thauanguimaraes20@gmail.com>
2023-11-16 19:30:21 -0300

    Projeto de uma Agência Bancária com Java e BD MySQL

---
+++ Agencia Bancaria/src/Programa/AgenciaBancaria.java
@@ -0,0 +1,160 @@
+package Programa;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AgenciaBancaria {
+  static Scanner sc = new Scanner(System.in);
+  static ArrayList<Conta> contasBancarias;
+
+  public static void main(String[] args) {
+    contasBancarias = new ArrayList<Conta>();
+    operacoes();
+  }
+  // Menu
+  public static void operacoes(){
+    System.out.println("------------------------------------------------------");
+    System.out.println("-------------Bem vindos a nossa Agência---------------");
+    System.out.println("------------------------------------------------------");
+    System.out.println("***** Selecione uma operação que deseja realizar *****");
+    System.out.println("------------------------------------------------------");
+    System.out.println("|   Opção 1 - Criar conta   |");
+    System.out.println("|   Opção 2 - Depositar     |");
+    System.out.println("|   Opção 3 - Sacar         |");
+    System.out.println("|   Opção 4 - Transferir    |");
+    System.out.println("|   Opção 5 - Listar        |");
+    System.out.println("|   Opção 6 - Sair          |");
+
+    int operacao = sc.nextInt();
+    sc.nextLine();
+
+    switch (operacao){
+      case 1: 
+        criarConta(); 
+        break;
+      case 2: 
+        depositar(); 
+        break;
+      case 3: 
+        sacar(); 
+        break;
+      case 4: 
+        transferir(); 
+        break;
+      case 5: 
+        listarContas();
+        break;
+      case 6: 
+        System.out.println("Obrigado por usar a nossa Agência"); 
+        System.exit(0);
+      default:
+        System.out.println("Opção inválida!");
+        operacoes();
+        break;
+   
+    }
+  }
+  // Mêtodo criar conta
+  public static void criarConta(){
+    System.out.println("\nNome: ");
+    String nome = sc.nextLine();
+    sc.nextLine();
+   
+    System.out.println("\nCPF: ");
+    String cpf = sc.nextLine();
+  
+    System.out.println("\nE_mail: ");
+    String email = sc.nextLine();
+  
+
+    Pessoa cliente = new Pessoa(nome, cpf, email);
+    Conta conta = new Conta(cliente);
+    contasBancarias.add(conta);
+    System.out.println("Sua conta foi criada com sucesso!");
+    operacoes();
+  }
+
+  // Mêtodo encontrar conta
+  private static Conta encontrarConta(int numeroConta) {
+    Conta conta = null;
+    if(contasBancarias.size() > 0) {
+        for(Conta contaa : contasBancarias) {
+            if(contaa.getNumeroConta() == numeroConta) {
+                conta = contaa;
+            }
+        }
+    }
+    return conta;
+}
+  // Mêtodo depositar
+  public static void depositar() {
+    System.out.println("Número da conta: ");
+    int numeroConta = sc.nextInt();
+    Conta conta = encontrarConta(numeroConta);
+
+    if(conta != null) {
+        System.out.println("Qual valor deseja depositar? ");
+        Double valorDeposito = sc.nextDouble();
+
+        conta.depositar(valorDeposito);
+    }else {
+        System.out.println("--- Conta não encontrada ---");
+    }
+    operacoes();
+}
+    // Mêtodo sacar
+    public static void sacar() {
+      System.out.println("Número da conta: ");
+      int numeroConta = sc.nextInt();
+
+      Conta conta = encontrarConta(numeroConta);
+
+      if(conta != null) {
+          System.out.println("Qual valor deseja sacar? ");
+          Double valorSaque = sc.nextDouble();
+
+          conta.sacar(valorSaque);
+      }else {
+          System.out.println("--- Conta não encontrada ---");
+      }
+
+      operacoes();
+
+  }
+    // Mêtodo transferir
+    public static void transferir(){
+      System.out.println("Informe o número da conta do remetente: ");
+      int numeroContaRemetente = sc.nextInt();
+      Conta contaRemetente = encontrarConta(numeroContaRemetente);
+      
+      // validação
+      if(contaRemetente != null){
+        System.out.println("Número da conta do destinatário: ");
+        int numeroContaDestinatario = sc.nextInt();
+        Conta contaDestinatario = encontrarConta(numeroContaDestinatario);
+
+        if (contaDestinatario != null) {
+          System.out.println("Quantia a ser transferida: ");
+          double valor = sc.nextDouble();
+          contaRemetente.transferir(contaDestinatario, valor);
+        }else{
+          System.out.println("Aconta para depósito não foi encontrada");
+        } 
+      }else{
+        System.out.println("Conta para transferir não encontrada");
+      }
+      operacoes();
+    }
+  
+  public static void listarContas(){
+    if (contasBancarias.size() > 0) {
+      // pecorrendo a lista
+      for(Conta conta : contasBancarias){
+        System.out.println(conta);
+      }
+    }else{
+      System.out.println("Sem contas cadastradas");
+    }
+    operacoes();
+  }
+}
--- Agencia Bancaria/src/Programa/Conta.java
+++ Agencia Bancaria/src/Programa/Conta.java
@@ -3,77 +3,81 @@ package Programa;
 import Utilitarios.Utils;
 
 public class Conta {
-  // Contador de contas
-  public static int contadorDeContas = 1;
-  // Atributos
+// contador
+  private static int accountCounter = 1;
+// Atributos
   private int numeroConta;
   private Pessoa pessoa;
-  private double saldo = 0.0;
-  // Construtor da Classe
+  private Double saldo = 0.0;
+
+// Construtor
   public Conta(Pessoa pessoa) {
-    this.numeroConta = contadorDeContas;
-    this.pessoa = pessoa;
-    contadorDeContas += 1;
+      this.numeroConta = Conta.accountCounter;
+      this.pessoa = pessoa;
+      this.updateSaldo();
+      Conta.accountCounter += 1;
   }
-  // Gets e Sets
+
+// Gets e Sets
   public int getNumeroConta() {
-    return numeroConta;
+      return numeroConta;
   }
-  public void setNumeroConta(int numeroConta) {
-    this.numeroConta = numeroConta;
+  public Pessoa getClient() {
+      return pessoa;
   }
-  public Pessoa getPessoa() {
-    return pessoa;
+  public void setClient(Pessoa pessoa) {
+      this.pessoa = pessoa;
   }
-  public void setPessoa(Pessoa pessoa) {
-    this.pessoa = pessoa;
+  public Double getSaldo() {
+      return saldo;
   }
-  public double getSaldo() {
-    return saldo;
-  }
-  public void setSaldo(double saldo) {
-    this.saldo = saldo;
+  public void setSaldo(Double saldo) {
+      this.saldo = saldo;
   }
 
-  // ToString
-  public String toString(){
-  return "\nNúmero da conta: " + this.getNumeroConta() +
-          "\nNome: " + this.pessoa.getNome() +
-          "\nCPF: " + this.pessoa.getCpf() +
-          "\nE_mail: " + this.pessoa.getEmail() +
-          "\nSaldo: " + Utils.doubleToString(this.getSaldo()) +
-          "\n";
+  private void updateSaldo() {
+      this.saldo = this.getSaldo();
+  }
+// ToString
+  public String toString() {
+
+      return "\nBank account: " + this.getNumeroConta() +
+              "\nCliente: " + this.pessoa.getName() +
+              "\nCPF: " + this.pessoa.getCpf() +
+              "\nEmail: " + this.pessoa.getEmail() +
+              "\nSaldo: " + Utils.doubleToString(this.getSaldo()) +
+              "\n" ;
+  }
+// Mêtodo depositar
+  public void depositar(Double valor) {
+      if(valor > 0) {
+          setSaldo(getSaldo() + valor);
+          //this.saldo = this.getSaldo() + valor;
+          System.out.println("Seu depósito foi realizado com sucesso!");
+      }else {
+          System.out.println("Não foi possível realizar o depósito!");
+      }
+  }
+  // Mêtodo sacar
+  public void sacar(Double valor) {
+      if(valor > 0 && this.getSaldo() >= valor) {
+          setSaldo(getSaldo() - valor);
+          System.out.println("Saque realizado com sucesso!");
+      }else {
+          System.out.println("Não foi possível realizar o saque!");
+      }
+  }
+  // Mêtodo transferir
+  public void transferir(Conta contaParaDeposito, Double valor) {
+      if(valor > 0 && this.getSaldo() >= valor) {
+          setSaldo(getSaldo() - valor);
+          //this.saldo = this.getSaldo() - valor;
+          contaParaDeposito.saldo = contaParaDeposito.getSaldo() + valor;
+          System.out.println("Transferência realizada com sucesso!");
+      }else {
+          System.out.println("Não foi possível realizar a tranferência");
+      }
+
   }
 
-  // Mêtodo de Depositar
-  public void depositar(double valor){
-    if(valor > 0){
-      setSaldo(getSaldo() + valor);
-      System.out.println("Depósito realizado com sucesso!");
-    }else{
-      System.out.println("Não foi possível realizar o depósito!");
-    }
-  }
-
-  // Mêtodo Sacar
-  public void sacar (double valor){
-    if(valor > 0 && this.getSaldo() >= valor){
-      setSaldo(getSaldo() - valor);
-      System.out.println("Saque realizado com sucesso!");
-    }else{
-      System.out.println("Não foi possível realizar o saque!");
-    }
-  }
-
-  // Mêtodo Transferir
-  public void transferir ( Conta contaParaDeposito, double valor){
-    if(valor > 0 && this.getSaldo() >= valor){
-      setSaldo(getSaldo() - valor);
-
-      contaParaDeposito.saldo = contaParaDeposito.getSaldo() + valor;
-      System.out.println("Transferência realizada com sucesso!");
-    }else{
-      System.out.println("Não foi possível realizar a transferência!");
-    }
-  }
 }
